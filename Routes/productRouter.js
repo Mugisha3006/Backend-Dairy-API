8fb444102f23031ba09c0c86b1aba456dc40a163
mugisha <mohamugi@gmail.com>
2024-09-24 16:17:47 +0300

    modify productRouter.js file

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { createProduct, getAllProducts, getProductById, deleteProduct, updateProduct } from '../Controllers/productController.js';
-import { createProduct, getAllProducts, getProductById, updateProductById, deleteProductById } from '../Controllers/productController.js';
 
 const router = Router();
 
 router.get('/allProducts', getAllProducts)
 
-router.get('/:id', getProductById)
+router.get('/product/:id', getProductById)
 
 router.post('/create', createProduct)
+
+router.put('/:id/updateProduct', updateProduct)
+
+router.delete('/:id/deleteProduct', deleteProduct)    
 
 export default router;
